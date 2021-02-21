efcbd34a8788d930075715aa43c47d50355656f3
crissdot <cris-2-11-2001@hotmail.com>
2021-02-21 15:11:17 -0600

    add drag listener

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 class DOMHelper {
     static move(el, coords) {
-        el.style.top = coords.y - (el.clientHeight / 2) + 'px';
+        el.style.top = coords.y - el.clientHeight + 'px';
         el.style.left = coords.x - (el.clientWidth / 2) + 'px';
     }
 
@@ -27,3 +27,79 @@ class DOMHelper {
         return -1;
     }
 }
+
+
+class DragList {
+    constructor(listSelector='ul', itemsSelector='li') {
+        this.list = document.querySelector(listSelector);
+        this.items = this.list.querySelectorAll(itemsSelector);
+        this.finalPosition = null;
+        this.finalElementHover = null;
+
+        this.handleDragStart = this.handleDragStart.bind(this);
+        this.handleDrag = this.handleDrag.bind(this);
+        this.handleDragEnd = this.handleDragEnd.bind(this);
+
+        this.canvas = document.createElement("canvas");
+
+        this.buildFakeElement();
+
+        this.addDragListeners();
+    }
+
+    buildFakeElement() {
+        this.fakeElement = document.createElement('div');
+        this.fakeElement.style.backgroundColor = '#eee';
+        this.fakeElement.classList.add('card');
+    }
+
+    addDragListeners() {
+        this.items.forEach(item => {
+            item.addEventListener("dragstart", this.handleDragStart);
+            item.addEventListener("drag", this.handleDrag);
+            item.addEventListener("dragend", this.handleDragEnd);
+        });
+    }
+
+    handleDragStart(event) {
+        let el = event.currentTarget;
+        event.dataTransfer.setDragImage(this.canvas, 0, 0);
+        el.classList.add("dragging");
+    }
+
+    handleDrag(event) {
+        let mouseCoords = {
+            x: event.clientX,
+            y: event.clientY,
+        };
+        DOMHelper.move(event.currentTarget, mouseCoords);
+
+        if(DOMHelper.isOver(this.list, mouseCoords)) {
+            this.items.forEach(item => this.compareElement(item, event));
+        }
+    }
+
+    compareElement(item, event) {
+        if(item == event.currentTarget) return;
+        let mouseCoords = {
+            x: event.clientX,
+            y: event.clientY,
+        };
+        let result = DOMHelper.whereIs(item, mouseCoords);
+        if(result === -1);
+
+        this.finalPosition = result;
+        this.finalElementHover = item;
+
+        if(result === 1) {
+            this.list.insertBefore(this.fakeElement, item.nextSibling);
+        }
+        if(result === 2) {
+            this.list.insertBefore(this.fakeElement, item);
+        }
+    }
+
+    handleDragEnd(event) {}
+}
+
+new DragList('.freestylers ul');
